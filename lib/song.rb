f73c5fc632a2dfef4191991d52c3ec1c9c6c1176
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-21 00:28:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,21 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create 
+    song.name = song
+  end
+    
 end
