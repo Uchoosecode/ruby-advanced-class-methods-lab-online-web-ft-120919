661fdb732119dc467e3df45e2d5fa4e1a2d0e17b
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-21 02:10:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,4 +44,10 @@ class Song
     song.artist_name = artist_name
     song
   end
+  
+  def self.create_from_filename(filename)
+    artist_name, song_name = filename.split(" - ")
+    song = self.create_by_name(song_name.chomp(".mp3"))
+    song.artist_name = artist_name
+    song
 end
