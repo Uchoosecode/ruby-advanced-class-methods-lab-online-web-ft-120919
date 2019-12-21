39961b44f945c46fca4b571ac5d071bde783bae0
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-21 01:27:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,10 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    
+    if song = self.find_by_name(name)
+      song
+    else 
+      self.create_by_name(name)
+    end
   end
 end
